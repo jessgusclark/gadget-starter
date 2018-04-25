54506f0af4f3c7214d4e976e4a45351a09f6e79e
Jesse Clark <jesse.clark@unco.edu>
2018-04-25 16:15:05 -0600

    Add stub for $.ajax call.

--- test/accounts-test.js
+++ test/accounts-test.js
@@ -8,11 +8,27 @@ global.jQuery = global.$ = require("jquery");
 var assert = require('chai').assert;
 var app = require('../source/js/accounts.js');
 
+//stubbed:
+global.gadget = {
+					token:"token",
+					account : "hello",
+					apihost: null
+				}
 
 describe('test accounts', function() {
 
-	it('should pull test account from stub', function(){
-		//assert.equal(1, 1 );
+	it('should pull test account data from stub', function(){
+		
+		// a sample response from ou campus:
+		var staticResponse = {"country":"US","institution":"University of Northern Colorado"}
+		// stub the a.jax call:
+		sinon.stub($, "ajax").returns($.Deferred().resolve( staticResponse ));
+
+		app.accounts.view().done(function(data){
+			assert.equal("US", data.country);
+			assert.equal("University of Northern Colorado", data.institution);
+		});
+
 	});
 
 });
